c4360896dd792f54ba709b1b9f47735b26cab0f8
ijritchey <ijritchey@hotmail.com>
2022-07-27 20:56:00 -0700

    added karen

--- js/app.js
+++ js/app.js
@@ -12,8 +12,6 @@ const foodOrder = document.querySelector('#foodOrder');
 const status = document.querySelector('#status');
 const score = document.querySelector('#score');
 
-
-let karen;
 let ingredient1;
 let ingredient2;
 let ingredient3;
@@ -32,18 +30,22 @@ class Character{
     }
 };
 
-const player = new Character(0,0, 60.8, 63, 0, 0, 9, false);
+const player = new Character(0, 0, 60.8, 63, 0, 0, 9, false);
+const karen = new Character(450, 200, 60, 60.5, 0, 0, false);
+
 
 const background = new Image();
 background.src = "/Users/ianritchey/Desktop/SEI-621/unit-1/deliverable/project-1-2d-game/imgs/pixil-frame-0 (1).png";
 const playerSprite = new Image();
-playerSprite.src = "/Users/ianritchey/Desktop/SEI-621/unit-1/deliverable/project-1-2d-game/imgs/Download59161 (3).png";
+playerSprite.src = "/Users/ianritchey/Desktop/SEI-621/unit-1/deliverable/project-1-2d-game/imgs/mainCharacter.png";
+const karenSprite = new Image();
+karenSprite.src = "/Users/ianritchey/Desktop/SEI-621/unit-1/deliverable/project-1-2d-game/imgs/karenCharacter.png"
 
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){ 
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 
-}
+};
 
 
 
@@ -51,19 +53,22 @@ function playerMovement(){
     if(keys['ArrowUp'] && player.y > 5) {
         player.y -= player.speed;
         player.frameY = 0;
+        player.moving = true;
     } ;
     if(keys['ArrowDown'] && player.y < game.height - player.height) {
         player.y += player.speed;
         player.frameY = 2;
+        player.moving = true;
     };
     if(keys['ArrowLeft'] && player.x > 0) {
         player.x -= player.speed;
         player.frameY = 1;
+        player.moving = true;
     };
     if(keys['ArrowRight'] && player.x < game.width - player.width) {
         player.x += player.speed;
         player.frameY = 3;
-
+        player.moving = true;
     }
 };
 
@@ -144,6 +149,7 @@ function animate() {
     ctx.drawImage(background, 0, 0, game.width, game.height);
     
     drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width*1.5, player.height*1.5);  
+    drawSprite(karenSprite, karen.width * karen.frameX, karen.height * karen.frameY, karen.width, karen.height, karen.x, karen.y, karen.width*1.5, karen.height*1.5);  
    
     playerMovement();
     handlePlayerFrame();
@@ -152,6 +158,7 @@ function animate() {
 }
 
 animate();
+
 
 // setInterval(function(){
 //     ctx.clearRect(0, 0, game.width, game.height);
